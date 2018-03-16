43655f22aedbf83ca98514be46fa7fed4165c53c
Wojtaszek <wojtaszekmichal@gmail.com>
2018-03-16 20:41:18 +0100

    Added Test Methods

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,19 @@
 public class StringCalculator {
 
     public int Add(String numbers) {
-        return 0;
+        String j = "";
+        if (numbers.equals(j)) {
+            return 0;
+        } else {
+            String list[] = null;
+            Integer newList[] = null;
+            list = numbers.split(",");
+            int summary = 0;
+            for (String i : list) {
+                summary += Integer.parseInt(i);
+            }
+            return summary;
+        }
     }
 }
+
