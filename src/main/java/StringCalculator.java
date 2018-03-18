1d25e0fa33e23cc877ea48d610ee1f3a47404ba0
maciejskro <maciejskro@gmail.com>
2018-03-18 12:00:13 +0100

    Maciek proposition

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,46 @@
 public class StringCalculator {
 
-    public int Add(String numbers) {
-        return 0;
+    public int add(String numbers) throws  NumberFormatException {
+        int a =0;
+        int b =0;
+
+
+        try {
+            if (numbers.matches("\\d*")) {
+                a= Integer.parseInt(numbers);
+                return a+b;
+            } else if(numbers.matches("\\d*,\\d*")) {
+                String[] table = numbers.trim().split(",");
+                a = Integer.parseInt(table[0]);
+                b = Integer.parseInt(table[1]);
+                return a+b;
+            } else if (numbers.matches("\\d*,-\\d*")) {
+                String[] table = numbers.trim().split(",");
+                a = Integer.parseInt(table[0]);
+                b = Integer.parseInt(table[1]);
+                return a+b;
+            } else if (numbers.matches("-\\d*,\\d*")) {
+                String[] table = numbers.trim().split(",");
+                a = Integer.parseInt(table[0]);
+                b = Integer.parseInt(table[1]);
+                return a+b;
+            } else if (numbers.matches("-\\d*,-\\d*")) {
+                String[] table = numbers.trim().split(",");
+                a = Integer.parseInt(table[0]);
+                b = Integer.parseInt(table[1]);
+                return a+b;
+            } else if( numbers.matches("[0-9, \\-]*")) {
+                String[] table = numbers.trim().split(",");
+                Integer sum = 0;
+                for (String s : table) {
+                    sum = sum + Integer.parseInt(s);
+                }
+                return sum;
+            }
+            else
+                return 0;
+        } catch (NumberFormatException e) {
+            return a+b;
+        }
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,6 +1,10 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * Tdd training on 16.03.18.
@@ -8,11 +12,93 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
 
-    @Test
-    public void shouldCreateObject() throws Exception {
-        StringCalculator testedObject = new StringCalculator();
-        assertThat(testedObject).isNotNull();
-        assertThat(testedObject.Add("")).isEqualTo(0);
+    private StringCalculator testedObject;
+    private Random random;
+    private static Integer stala=10000;
+
+    @Before
+    public void setUp() {
+        testedObject = new StringCalculator();
+        random = new Random();
     }
 
+    @Test
+    public void shouldCreateObject() throws Exception {
+
+        assertThat(testedObject).isNotNull();
+        assertThat(testedObject.add("")).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldShowZeroForParameterZero() throws  Exception{
+
+        int result = testedObject.add("0");
+
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldRetrunNumberIfOneNumberIsGiven() throws Exception {
+
+        int result = testedObject.add("1");
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public  void shouldReturnNumberIfNumberisGiven() throws Exception {
+
+
+        int test  = random.nextInt(1000000);
+        int result = testedObject.add(Integer.toString(test));
+
+        assertThat(result).isEqualTo(test);
+    }
+
+    @Test
+    public void shouldReturnSumOfTwoNumberWithColon() {
+        int constant = 1000;
+        int test1 = random.nextInt(constant);
+        int test2 = random.nextInt(constant);
+        int result = testedObject.add( Integer.toString(test1)+","+Integer.toString(test2));
+
+        assertThat(result).isEqualTo(test1+test2);
+    }
+
+    @Test
+    public  void shouldReturnSumOfSecondNegative() {
+
+        int test1 = random.nextInt(stala);
+        int test2 = random.nextInt(stala)-stala;
+        int result = testedObject.add(Integer.toString(test1)+"," + Integer.toString(test2));
+        assertThat(result).isEqualTo(test1+test2);
+    }
+
+    @Test
+    public  void shouldReturnSumOfFirstNegative() {
+
+        int test1 = random.nextInt(stala)-stala;
+        int test2 = random.nextInt(stala);
+        int result = testedObject.add(Integer.toString(test1)+"," + Integer.toString(test2));
+        assertThat(result).isEqualTo(test1+test2);
+    }
+
+    @Test
+    public  void shouldReturnSumOfTwoNegative() {
+
+        int test1 = random.nextInt(stala)-stala;
+        int test2 = random.nextInt(stala)-stala;
+        int result = testedObject.add(Integer.toString(test1)+"," + Integer.toString(test2));
+        assertThat(result).isEqualTo(test1+test2);
+    }
+
+    @Test
+    public  void shouldReturnSumOfThreElement() {
+
+        int test1 = random.nextInt(stala*2)-stala;
+        int test2 = random.nextInt(stala*2)-stala;
+        System.out.println("test1: " +test1 + " test2: " + test2 );
+        int result = testedObject.add(Integer.toString(test1)+"," + Integer.toString(test2) + "," + Integer.toString(test1*test2));
+        assertThat(result).isEqualTo(test1+test2+(test1*test2));
+    }
 }
